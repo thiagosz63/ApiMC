029f4ca56b01cf06cd3ba0602bdd509cf4784f36
thiagosz63 <thiagosz63@gmail.com>
2021-10-06 12:10:01 -0300

    Project Created

---
+++ src/main/java/com/tsa/ApiMC/ApiMcApplication.java
@@ -0,0 +1,13 @@
+package com.tsa.ApiMC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiMcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiMcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tsa/ApiMC/ApiMcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tsa.ApiMC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiMcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
