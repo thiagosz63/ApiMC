1831af455459e9d988676f9b507e593e95e3f5f3
thiagosz63 <thiagosz63@gmail.com>
2021-10-20 21:09:59 -0300

    testing domain first class - category

--- src/main/java/com/tsa/ApiMC/controller/CategoryController.java
+++ src/main/java/com/tsa/ApiMC/controller/CategoryController.java
@@ -1,15 +1,26 @@
 package com.tsa.ApiMC.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.tsa.ApiMC.entities.Category;
 
 @RestController
 @RequestMapping(value = "/categories")
 public class CategoryController {
 
 	@GetMapping
-	public String listar() {
-		return "rest esta fucionando";
+	public List<Category> listar() {
+		
+		Category cat1 = new Category(1, "informatica");
+		Category cat2 = new Category(2, "escritorio");
+		List<Category> list = new ArrayList<>();
+		list.add(cat1);
+		list.add(cat2);
+		return list;
 	}
 }
