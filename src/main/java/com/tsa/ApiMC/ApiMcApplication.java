bb3261ec394460875d919cf46f72512834eea14d
Thiagosz <thiagosz63@gmail.com>
2021-11-01 03:18:09 -0300

    State and City

--- src/main/java/com/tsa/ApiMC/ApiMcApplication.java
+++ src/main/java/com/tsa/ApiMC/ApiMcApplication.java
@@ -8,9 +8,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.tsa.ApiMC.entities.Category;
+import com.tsa.ApiMC.entities.City;
 import com.tsa.ApiMC.entities.Product;
+import com.tsa.ApiMC.entities.State;
 import com.tsa.ApiMC.repository.CategoryRepository;
+import com.tsa.ApiMC.repository.CityRepository;
 import com.tsa.ApiMC.repository.ProductRepository;
+import com.tsa.ApiMC.repository.StateRepository;
 
 @SpringBootApplication
 public class ApiMcApplication implements CommandLineRunner {
@@ -20,6 +24,13 @@ public class ApiMcApplication implements CommandLineRunner {
 	
 	@Autowired
 	private ProductRepository productReposytory;
+	
+	@Autowired
+	private CityRepository cityReposytory;
+	
+	@Autowired
+	private StateRepository stateReposytory;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ApiMcApplication.class, args);
 	}
@@ -43,6 +54,21 @@ public class ApiMcApplication implements CommandLineRunner {
 		
 		categoryReposytory.saveAll(Arrays.asList(cat1,cat2));
 		productReposytory.saveAll(Arrays.asList(p1,p2,p3));
+		
+		State est1 = new State(null, "Minas Gerais");
+		State est2 = new State(null, "São Paulo");
+		
+		City c1 = new City(null, "Urberlandia", est1);
+		City c2 = new City(null, "São Paulo", est2);
+		City c3 = new City(null, "Campinas", est2);
+		
+		est1.getCities().addAll(Arrays.asList(c1));
+		est2.getCities().addAll(Arrays.asList(c2,c3));
+		
+		stateReposytory.saveAll(Arrays.asList(est1,est2));
+		cityReposytory.saveAll(Arrays.asList(c1,c2,c3));
+		
+	
 		
 	}
 
