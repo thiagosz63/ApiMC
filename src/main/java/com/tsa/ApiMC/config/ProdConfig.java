f11cd737a5688a9610b839eeb34339c45f54d643
thiagosz63 <thiagosz63@gmail.com>
2022-04-18 22:24:32 -0300

    add configuration for production

---
+++ src/main/java/com/tsa/ApiMC/config/ProdConfig.java
@@ -0,0 +1,39 @@
+package com.tsa.ApiMC.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.tsa.ApiMC.service.DBService;
+import com.tsa.ApiMC.service.EmailService;
+import com.tsa.ApiMC.service.SmtpEmailService;
+
+@Configuration
+@Profile("prod")
+public class ProdConfig {
+
+	@Autowired
+	DBService dbService;
+
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+
+	@Bean
+	public Boolean instantiateDataBase() throws ParseException {
+
+		if (!"create".equals(strategy)) {
+			return false;
+		}
+		dbService.instantiateTestDataBase();
+		return true;
+	}
+
+	@Bean
+	public EmailService emailService() {
+		return new SmtpEmailService();
+	}
+}
